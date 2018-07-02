b1745f5422a3518ae6974c83126cbb170091e0cc
Iván Ávalos <ivan.avalos.diaz@hotmail.com>
2018-07-02 13:36:11 -0500

    Descendant order in Home and Tags

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,32 +26,30 @@ class HomeController extends Controller
      */
     public function index()
     {
-	$links = DB::table('links')->where('user_id',
-    Auth::id())->simplePaginate(15);
-	foreach($links as $link) {
-	    $link->tags = explode(" ", $link->tags);
-	}
-        return view('home', ['links'=>$links]);
+    	$links = DB::table('links')->where('user_id', Auth::id())->orderBy('id', 'desc')->simplePaginate(15);
+    	foreach($links as $link) {
+    		$link->tags = explode(" ", $link->tags);
+    	}
+    	return view('home', ['links'=>$links]);
     }
+    
     public function edit($id)
     {
-	$link = DB::table('links')->where('id', $id)->get();
-	if(sizeof($link) == 0) return '<h1>Link not found</h1>';
-	if($link[0]->user_id != Auth::id()) return '<h1>Unauthorized</h1>';
-	return view('edit', ['link'=>$link[0]]);
+    	$link = DB::table('links')->where('id', $id)->get();
+    	if(sizeof($link) == 0) return '<h1>Link not found</h1>';
+    	if($link[0]->user_id != Auth::id()) return '<h1>Unauthorized</h1>';
+    	return view('edit', ['link'=>$link[0]]);
     }
 
-    public function tags($tag)
-    {
-	$links = DB::table('links')->where('user_id',
-    Auth::id())->get();
-        $filtered_links = [];
-	foreach($links as $link) {
-	    $link->tags = explode(" ", $link->tags);
-	    if(in_array($tag, $link->tags))
-	        array_push ($filtered_links, $link);
-	}
-	return view('home', ['links'=>$filtered_links, 'no_add'=>true, 'pagination'=>false,
-    'title'=>'Tag: '.$tag]);
-    }
+    public function tags($tag){
+    	$links = DB::table('links')->where('user_id', Auth::id())->orderBy('id', 'desc')->get();
+    	$filtered_links = [];
+    	foreach($links as $link) {
+    		$link->tags = explode(" ", $link->tags);
+    		if(in_array($tag, $link->tags))
+    		array_push ($filtered_links, $link);
+    	}
+    	return view('home', ['links'=>$filtered_links, 'no_add'=>true, 'pagination'=>false,
+    	'title'=>'Tag: '.$tag]);
+    }    	  	
 }
