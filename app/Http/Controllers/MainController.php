01ba0dbfa97dd882533924fe38def6fbf225286a
Iván Ávalos <ivan.avalos.diaz@hotmail.com>
2018-07-25 22:09:40 -0500

    Fixed some

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,26 +12,23 @@ class MainController extends Controller
     //
     function add(Request $request) {
         $title = $request->input('title');
-	$link = $request->input('link');
-	$tags = $request->input('tags');
-	DB::table('links')->insert(
-	    ['user_id' => Auth::id(), 'link'=>$link, 'title'=>$title, 'tags'=>$tags]
-	);
+        $link = $request->input('link');
+        $tags = $request->input('tags');
+        DB::table('links')->insert(['user_id' => Auth::id(), 'link'=>$link, 'title'=>$title, 'tags'=>$tags]);
         return Redirect::route('home');
     }
     function remove($id) {
         DB::table('links')->where('user_id', Auth::id())->where('id', $id)->delete();
-	return Redirect::route('home');
+        return Redirect::route('home');
     }
     function update($id, Request $request) {
         $title = $request->input('title');
-	$link = $request->input('link');
-	$tags = $request->input('tags');
+        $link = $request->input('link');
+        $tags = $request->input('tags');
         $results = DB::table('links')->where('id', $id)->get();
-	if(sizeof($results) <= 0) return 'Link not found';
-	if($results[0]->user_id != Auth::id()) return '<h1>Unauthorized</h1>';
-	DB::table('links')->where('id', $id)->update(['title'=>$title,
-	'link'=>$link, 'tags'=>$tags]);
-	return Redirect::route('home');
+        if(sizeof($results) <= 0) return 'Link not found';
+        if($results[0]->user_id != Auth::id()) return '<h1>Unauthorized</h1>';
+        DB::table('links')->where('id', $id)->update(['title'=>$title, 'link'=>$link, 'tags'=>$tags]);
+        return Redirect::route('home');
     }
 }
